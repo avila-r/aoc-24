1ec3dd261dacad6120009d245b0e141253e702df
r. ávila <avila.dev@outlook.com>
2024-12-16 13:14:41 -0300

    feat: implement day 2 solution

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,32 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  import Enum
+
+  def part1(input) do
+    input
+    |> filter(&safe?/1)
+    |> count()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    IO.inspect(input)
+
+    input
+    |> filter(fn report ->
+      safe?(report) or
+        any?(0..(length(report) - 1), fn i ->
+          report
+          |> List.delete_at(i)
+          |> safe?()
+        end)
+    end)
+    |> count()
+  end
+
+  defp safe?(report) do
+    (sort(report) == report or
+       sort(report, :desc) == report) and
+      report
+      |> chunk_every(2, 1, :discard)
+      |> all?(fn [a, b] -> abs(a - b) in 1..3 end)
   end
 end
