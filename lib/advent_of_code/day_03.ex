f5cd6e32d83a5b645c6e0912cd3c207654c5e649
r. ávila <avila.dev@outlook.com>
2024-12-16 17:04:20 -0300

    feat: implement day 3 solution

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,31 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  import Enum
+  import String
+
+  def part1(memory) do
+    ~r/mul\((\d{1,3}),(\d{1,3})\)/
+    |> Regex.scan(memory, capture: :all_but_first)
+    |> reduce(0, fn [a, b], acc ->
+      acc + to_integer(a) * to_integer(b)
+    end)
   end
 
-  def part2(_args) do
+  def part2(memory) do
+    ~r/(mul)\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/
+    |> Regex.scan(memory)
+    |> Stream.transform(:do, fn
+      ["do()"], _state ->
+        {[], :do}
+
+      ["don't()"], _state ->
+        {[], :dont}
+
+      [_, "mul", a, b], :do ->
+        {[to_integer(a) * to_integer(b)], :do}
+
+      [_, "mul", _a, _b], :dont ->
+        {[], :dont}
+    end)
+    |> sum()
   end
 end
