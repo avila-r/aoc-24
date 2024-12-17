afcb70a8f8999bdff6e4f91ebd456ecdeaab4029
r. ávila <avila.dev@outlook.com>
2024-12-16 23:28:53 -0300

    feat: implement day 4 solution

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,61 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  import Enum
+
+  @directions [
+    {-1, -1},
+    {-1, 0},
+    {-1, 1},
+    {0, -1},
+    {0, 1},
+    {1, -1},
+    {1, 0},
+    {1, 1}
+  ]
+
+  @diagonals [
+    {-1, -1},
+    {-1, 1},
+    {1, -1},
+    {1, 1}
+  ]
+
+  def part1(input) do
+    input
+    |> reduce(0, fn {position, _}, acc ->
+      @directions
+      |> reduce(acc, fn direction, acc ->
+        case matches?(input, ~c"XMAS", position, direction) do
+          true -> acc + 1
+          false -> acc
+        end
+      end)
+    end)
   end
 
-  def part2(_args) do
+  defp matches?(grid, [expected | remaining], {row, col}, {s_row, s_col}) do
+    case Map.get(grid, {row, col}) do
+      ^expected -> matches?(grid, remaining, {row + s_row, col + s_col}, {s_row, s_col})
+      _ -> false
+    end
+  end
+
+  defp matches?(_, [], _, _), do: true
+
+  def part2(input) do
+    mas? = fn grid, {x, y}, {dx, dy} ->
+      Map.get(grid, {x + dx, y + dy}) == ?M and
+        Map.get(grid, {x, y}) == ?A and
+        Map.get(grid, {x - dx, y - dy}) == ?S
+    end
+
+    input
+    |> reduce(0, fn {position, _}, acc ->
+      acc +
+        (@diagonals
+         |> count(fn {x, y} ->
+           mas?.(input, position, {x, y}) and
+             mas?.(input, position, {y, -x})
+         end))
+    end)
   end
 end
