dabe228cf7a35bbe7d53e481f6a3a767d87d9573
r. ávila <avila.dev@outlook.com>
2024-12-20 11:47:45 -0300

    test: implement day 7 default cases

--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,19 +3,19 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+         190: 10 19
+         3267: 81 40 27
+         83: 17 5
+         156: 15 6
+         7290: 6 8 6 15
+         161011: 16 10 13
+         192: 17 8 14
+         21037: 9 7 18 13
+         292: 11 6 16 20
+         """
+         |> AdventOfCode.Input.Day07.parse()
 
-    assert result
-  end
-
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
-  end
+  test "part1", do: assert(part1(@input) == 3749)
+  test "part2", do: assert(part2(@input) == 11387)
 end
