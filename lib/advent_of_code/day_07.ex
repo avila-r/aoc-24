d8bdea4b64483e5695980e55e38cd92ff5c8a17e
r. ávila <avila.dev@outlook.com>
2024-12-20 11:47:32 -0300

    feat: implement day 7 solutions

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,34 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(input, opts \\ [concat: false]) do
+    input
+    |> Enum.flat_map(fn {expected, [head | tail]} ->
+      if matches?(head, tail, expected, opts |> Keyword.get(:concat, false)),
+        do: [expected],
+        else: []
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  defp matches?(head, [h | t], expected, concat) do
+    cond do
+      matches?(head + h, t, expected, concat) ->
+        true
+
+      matches?(head * h, t, expected, concat) ->
+        true
+
+      true ->
+        concat and matches?(concat(head, h) + h, t, expected, concat)
+    end
+  end
+
+  defp matches?(n, [], expected, _), do: n === expected
+
+  defp concat(a, 0), do: a
+  defp concat(a, b), do: concat(a * 10, div(b, 10))
+
+  def part2(input) do
+    input
+    |> part1(concat: true)
   end
 end
