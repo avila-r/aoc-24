4c0f88035392c1fc20b06855bfdcaf6f0f1dbcf9
r. ávila <avila.dev@outlook.com>
2024-12-22 11:01:54 -0300

    feat: implement day 10 solutions

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,30 @@
 defmodule AdventOfCode.Day10 do
-  def part1(_args) do
+  import Enum
+
+  def part1(map) do
+    map
+    |> filter(fn {_, h} -> h === 0 end)
+    |> map(fn {position, 0} -> goals(map, position, 1) end)
+    |> map(fn goal -> goal |> uniq() |> count() end)
+    |> sum()
   end
 
-  def part2(_args) do
+  def part2(map) do
+    map
+    |> filter(fn {_, height} -> height === 0 end)
+    |> map(fn {position, 0} -> goals(map, position, 1) end)
+    |> map(&count/1)
+    |> sum()
+  end
+
+  defp goals(map, {r, c}, h) do
+    [{r - 1, c}, {r, c - 1}, {r, c + 1}, {r + 1, c}]
+    |> flat_map(fn n ->
+      case map do
+        %{^n => 9} when h === 9 -> [n]
+        %{^n => ^h} -> goals(map, n, h + 1)
+        %{} -> []
+      end
+    end)
   end
 end
