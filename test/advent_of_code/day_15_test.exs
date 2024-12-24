a6f9b2c652f9e31cfd44113966e1e4e0e343278f
r. ávila <avila.dev@outlook.com>
2024-12-24 15:53:39 -0300

    test: implement day 15 default cases

--- test/advent_of_code/day_15_test.exs
+++ test/advent_of_code/day_15_test.exs
@@ -1,21 +1,52 @@
 defmodule AdventOfCode.Day15Test do
   use ExUnit.Case
 
-  import AdventOfCode.Day15
+  import AdventOfCode.{Input.Day15, Day15}
 
-  @tag :skip
+  @smaller """
+           ########
+           #..O.O.#
+           ##@.O..#
+           #...O..#
+           #.#.O..#
+           #...O..#
+           #......#
+           ########
+
+           <^^>>>vv<v>>v<<
+           """
+           |> parse()
+
+  @larger """
+          ##########
+          #..O..O.O#
+          #......O.#
+          #.OO..O.O#
+          #..O@..O.#
+          #O#..O...#
+          #O..O..O.#
+          #.OO.O.OO#
+          #....O...#
+          ##########
+
+          <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+          vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+          ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+          <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+          ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+          ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+          >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+          <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+          ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+          v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+          """
+          |> parse()
+
   test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+    assert part1(@smaller) == 2028
+    assert part1(@larger) == 10092
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
   end
 end
