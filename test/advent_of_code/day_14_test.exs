294a8a8fc8d9ff2146c3059c641a1db7320b13b4
r. ávila <avila.dev@outlook.com>
2024-12-24 11:27:00 -0300

    test: implement day 14 default cases

--- test/advent_of_code/day_14_test.exs
+++ test/advent_of_code/day_14_test.exs
@@ -1,21 +1,24 @@
 defmodule AdventOfCode.Day14Test do
   use ExUnit.Case
 
-  import AdventOfCode.Day14
+  import AdventOfCode.{Input.Day14, Day14}
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+         p=0,4 v=3,-3
+         p=6,3 v=-1,-3
+         p=10,3 v=-1,2
+         p=2,0 v=2,-1
+         p=0,0 v=1,3
+         p=3,0 v=-2,-2
+         p=7,6 v=-1,-3
+         p=3,0 v=-1,-2
+         p=9,3 v=2,3
+         p=7,3 v=-1,2
+         p=2,4 v=2,-3
+         p=9,5 v=-3,-3
+         """
+         |> parse()
 
-    assert result
-  end
-
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
-  end
+  test "part1", do: assert(@input |> part1() == 12)
+  test "part2", do: @input |> IO.inspect(label: "Input")
 end
