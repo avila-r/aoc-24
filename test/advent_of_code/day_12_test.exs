a238514c734bba7449adebc82dd3f735e8b540cd
r. ávila <avila.dev@outlook.com>
2024-12-23 21:59:25 -0300

    test: implement day 12 default cases

--- test/advent_of_code/day_12_test.exs
+++ test/advent_of_code/day_12_test.exs
@@ -3,19 +3,20 @@ defmodule AdventOfCode.Day12Test do
 
   import AdventOfCode.Day12
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+         RRRRIICCFF
+         RRRRIICCCF
+         VVRRRCCFFF
+         VVRCCCJFFF
+         VVVVCJJCFE
+         VVIVCCJJEE
+         VVIIICJJEE
+         MIIIIIJJEE
+         MIIISIJEEE
+         MMMISSJEEE
+         """
+         |> AdventOfCode.Input.Day12.parse()
 
-    assert result
-  end
-
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
-  end
+  test "part1", do: assert(@input |> part1() == 1930)
+  test "part2", do: assert(@input |> part2() == 1206)
 end
